7f384bca4566dce38b390b925aa17d6496f21246
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-29 05:40:59 +0530

    create a design and responsive for mobile screen

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+let img = document.getElementById("clk-Img")
+let checkBtn= document.getElementById("checkValue")
+
+let body = document.getElementById("body")
+
+function chnageImage(color){
+
+     switch(color){
+         
+    case 'orange':
+        img.src=`images/orange.png`
+        break;
+
+    case 'white':
+        img.src=`images/white.png`
+    break; 
+
+    case 'skin':
+        img.src=`images/skin.png`
+    break;
+
+    default:
+        img.src=`images/green.png`
+    break;
+
+
+         
+     }
+
+}
+
+checkBtn.addEventListener('click',function(e){
+
+     if(checkBtn.checked){
+        body.classList.remove("white-theam")
+        body.classList.add("black-theam")
+     }else{
+        body.classList.remove("black-theam")
+        body.classList.add("white-theam")
+     }
+})
+
+
